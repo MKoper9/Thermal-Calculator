9dd42187251645defc336aaf0a9147cda57f9b4d
Michal <mkoperpno@gmail.com>
2023-12-03 21:15:29 +0100

    add exceptions and database configuration

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+  'db' => [
+    'host' => 'localhost',
+    'database' => 'calculator',
+    'user' => 'user',
+    'password' => 'fcv3iRcTxYfnNY6H'
+  ]
+];
---
+++ templates/pages/list.php
@@ -0,0 +1,16 @@
+<div class="list">
+
+    <div class="tbl-header">
+        <table cellpadding="0" cellspacing="0" border="0">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Material</th>
+                    <th>Gęstość</th>
+                    <th>Współczynnik przewodzenia ciepła</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+
+</div>
