f9970f9dd36d63a742fce3b87341daa37687e288
Michal <mkoperpno@gmail.com>
2023-12-06 20:02:51 +0100

    create new models

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Request;
+
 $configuration = require_once("config/config.php");
 
 $request = new Request($_GET, $_POST, $_SERVER);
--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -4,12 +4,54 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\View;
+use App\Request;
+use App\Exception\StorageException;
+use App\Exception\NotFoundException;
+use App\Exception\ConfigurationException;
+use CalculatorModel;
+
 abstract class AbstractController
 {
     private static array $configuration = [];
 
+    protected View $view;
+    protected Request $request;
+
+    public function __construct(Request $request)
+    {
+        if (empty(self::$configuration['db'])) {
+            throw new ConfigurationException('Configuration error');
+        }
+        $this->calculatorModel = new CalculatorModel(self::$configuration['db']);
+
+        $this->request = $request;
+        $this->view = new View();
+    }
+
+    final public function run(): void
+    {
+        try {
+            $action = $this->action() . 'Action';
+            if (!method_exists($this, $action)) {
+                $action = '' . 'Action';
+            }
+            $this->$action();
+        } catch (StorageException $e) {
+            // Log::error($e->getPrevios());
+            $this->view->render('error', ['message' => $e->getMessage()]);
+        } catch (NotFoundException $e) {
+            $this->redirect('/', ['error' => 'noteNotFound']);
+        }
+    }
+
     public static function initConfiguration(array $configuration): void
     {
         self::$configuration = $configuration;
+    }
+
+    final private function action(): string
+    {
+        return $this->request->getParam('action', '');
     }
 }
