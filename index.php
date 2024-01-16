7dd0e77587d4d5b1f4452a75f7fd1f8ff124fbfd
Michal <mkoperpno@gmail.com>
2024-01-16 23:33:43 +0100

    first commit

--- index.php
+++ index.php
@@ -2,31 +2,4 @@
 
 declare(strict_types=1);
 
-spl_autoload_register(function (string $classNamespace) {
-    $path = str_replace(['\\', 'App/'], ['/', ''], $classNamespace);
-    $path = "src/$path.php";
-    require_once($path);
-});
-
-use App\Request;
-use App\Exception\AppException;
-use App\Controller\AbstractController;
-use App\Controller\CalculatorController;
-use App\Exception\ConfigurationException;
-
-$configuration = require_once("config/config.php");
-
-$request = new Request($_GET, $_POST, $_SERVER);
-
-try {
-    AbstractController::initConfiguration($configuration);
-    (new CalculatorController($request))->run();
-} catch (ConfigurationException $e) {
-    echo '<h1>Wystąpił błąd w aplikacji</h1>';
-    echo 'Problem z applikacją, proszę spróbować za chwilę.';
-} catch (AppException $e) {
-    echo '<h1>Wystąpił błąd w aplikacji</h1>';
-    echo '<h3>' . $e->getMessage() . '</h3>';
-} catch (\Throwable $e) {
-    echo '<h1>Wystąpił błąd w aplikacji</h1>';
-}
+echo "<h1>Test<h1>";
